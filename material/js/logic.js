6abc281c90ff3021542e662d9220feb7be0675e0
Denis Mylnikov <denismylnikov@MacBook-Pro-Denis.local>
2017-08-22 20:37:16 +0200

    Moved material design version completely into different folder

---
+++ material/js/logic.js
@@ -0,0 +1,311 @@
+
+var weight;
+var age;
+var height;
+var days = 0;
+var calorieIntake;
+var goalWeight;
+var activityLevel;
+var female = false;
+var date = new Date();
+var checkPassed = true;
+var labels = []; //this needs to be cleared before filling again
+var data = []; //this needs to be cleared before filling again
+
+//clicking the button calls this
+function calculate() {
+
+  var t0 = performance.now();
+
+  checkPassed = true; //otherwise value will be permanently stuck in false if one of the checks failed
+
+  //showStartingData();
+  readInputValues();
+  checkValues();
+  checkIntake();
+
+  //this doesn't work for some reason
+  if(checkPassed) {
+    myApp.showPreloader();
+    calculateWeightLoss();
+  }
+
+  var t1 = performance.now();
+  console.log("Calculation took: " + (t1-t0).toFixed(3) + " ms");
+}
+
+//Adds specified number of days to current date
+function addDays(days) {
+  date.setDate(date.getDate() + days);
+}
+
+/*function showStartingData() {
+  console.log("*STARTING PARAMETERS*");
+  console.log("TDEE is: " + calculateTDEE());
+  console.log("Current BMI is: " + calculateBMI().toFixed(1));
+  console.log("Goal weight is probably: " + calculateGoalWeight());
+  console.log("Actual activity level: " + activityLevel);
+  console.log("--------------------------");
+}*/
+
+function readInputValues() {
+
+  //assign all text input fields to variables
+  weight = parseFloat(document.getElementById("currentWeightInput").value);
+  age = parseFloat(document.getElementById("ageInput").value);
+  height = parseFloat(document.getElementById("heightInput").value);
+  calorieIntake = parseFloat(document.getElementById("calorieIntakeInput").value);
+  goalWeight = parseFloat(document.getElementById("goalWeightInput").value);
+
+  //get selected gender
+  var genderSelector = document.getElementById("genderSelect");
+
+  if (genderSelector.options[genderSelector.selectedIndex].value == "Female") {
+    female = true;
+  } else {
+    female = false;
+  }
+
+  //get activity level
+  activityLevel = parseInt(getActivityLevel());
+
+}
+
+//checks input and throws errors in case some values are inadequate
+function checkValues() {
+  if (weight < 40 || weight > 600) {
+    myApp.alert("Weight must be between 40 and 600.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (weight == null || weight == "" || isNaN(weight)) {
+    myApp.alert("Weight input cannot be empty!", "Oops!");
+    checkPassed = false;
+  }
+
+  if (age == null || age == "" || isNaN(age)) {
+    myApp.alert("Age input cannot be empty!", "Oops!");
+    checkPassed = false;
+  }
+
+  if (age < 10 || age > 110) {
+    myApp.alert("Age must be between 10 and 110.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (height == null || height == "" || isNaN(height)) {
+    myApp.alert("Height cannot be empty.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (height < 100 || height > 250) {
+    myApp.alert("Height must be between 100 and 250.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (goalWeight > weight) {
+    myApp.alert("Goal weight must be lower than current weight.", "Oops!");
+    checkPassed = false;
+  }
+
+  //there needs to be a functionality that calculates goal weight based on BMI if field is left empty
+  if (goalWeight == null || goalWeight == "" || isNaN(goalWeight)) {
+    myApp.alert("Goal weight cannot be empty.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (goalWeight < 30 || goalWeight > 500) {
+    myApp.alert("Goal weight must be between 30 and 500.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (calorieIntake < 1000) {
+    myApp.alert("Calorie intake of less than 1000 is very unhealthy, please don't do it.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (calorieIntake > 15000) {
+    myApp.alert("Calorie intake can't be higher than 15000.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (calorieIntake == null || calorieIntake == "" || isNaN(calorieIntake)) {
+    myApp.alert("Calorie intake can't be empty!", "Oops!");
+    checkPassed = false;
+  }
+
+}
+
+function checkIntake() {
+  if (calculateTDEE() < calorieIntake) {
+    myApp.alert("You calorie intake is too high to lose weight. Reduce intake or increase excersise.", "Oops!");
+    checkPassed = false;
+  }
+}
+
+function getActivityLevel() {
+  var radios = document.getElementsByName("my-radio");
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      return radios[i].value;
+      break;
+    }
+  }
+
+}
+
+function calculateTDEE() {
+
+  var bmr;
+
+  if(female) {
+    bmr = (10*weight) + (6.25*height) - (5*age) - 161; //<-- this happens way too many times, optimize
+  } else {
+    bmr = (10*weight) + (6.25*height) - (5*age) + 5; //<-- this happens way too many times, optimize
+  }
+
+  var tdee = 0;
+
+  switch (activityLevel) {
+    case 1: tdee = parseInt(bmr*1.20);
+    break;
+
+    case 2: tdee = parseInt(bmr*1.375);
+    break;
+
+    case 3: tdee = parseInt(bmr*1.550);
+    break;
+
+    case 4: tdee = parseInt(bmr*1.725);
+    break;
+
+    case 5: tdee = parseInt(bmr*1.900);
+    break;
+
+    default: tdee = parseInt(bmr*1.20);
+    break;
+
+  }
+
+  return tdee;
+
+}
+
+//this works correctly
+function calculateBMI() {
+  return (weight/(height ** 2))*10000;
+}
+
+function calculateGoalWeight() {
+  return (height ** 2)*23/10000;
+}
+
+function calculateWeightLoss() {
+
+  labels = [];
+  data = [];
+
+  date = new Date();
+
+  //these do 2 times a month
+  /*labels.push(date.getDate() + "/" + parseMonth(date.getMonth()) + "/" + date.getFullYear());
+  data.push(weight.toFixed(1));*/
+
+  //these do 1 time per month
+  labels.push(parseMonth(date.getMonth()) + " " + date.getFullYear());
+  data.push(weight.toFixed(1));
+
+while(parseFloat(weight) >= parseFloat(goalWeight-3)) {
+
+  var deficit = calculateTDEE() - calorieIntake;
+  var kgDeficit = deficit/7700;
+  weight = weight - kgDeficit;
+  addDays(1);
+  age = age + 1/365;
+
+  //this does two times a month
+  /*if(date.getDate() == 1 || date.getDate() == 14) {
+    console.log("TDEE is: " + calculateTDEE());
+		console.log("Current age: " + age);
+		console.log("Day " + days + ": Current weight is: " + weight);
+    console.log("The date is: " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear());
+    console.log("---------------------")
+    labels.push(date.getDate() + "/" + parseMonth(date.getMonth()) + "/" + date.getFullYear());
+    data.push(weight.toFixed(1));
+  }*/
+
+  //on the 14th day of each month add data to arrays that will be displayed on chart
+  if(date.getDate() == 14) {
+    labels.push(parseMonth(date.getMonth()) + " " + date.getFullYear());
+    data.push(weight.toFixed(1));
+  }
+
+  if (calculateTDEE() <= calorieIntake) {
+        myApp.alert("You can't reach your goal weight with given calorie intake and level of activity. The lowest weight you can achieve is: " + weight.toFixed(1), "Oops!");
+        weight = 0; //this needs to be here to break out of while loop
+			}
+
+    }
+
+    //load about page that contains the chart
+    getMainView().router.loadPage('about.html');
+
+    //draw chart even if we aren't on the index page
+    if (getMainView().activePage.name != "index-1") {
+      drawChart();
+    }
+
+}
+
+function getLabels() {
+  return labels;
+}
+
+function getData() {
+  return data;
+}
+
+function parseMonth(month) {
+  switch (month) {
+    case 0: return "Jan";
+    break;
+
+    case 1: return "Feb";
+    break;
+
+    case 2: return "Mar";
+    break;
+
+    case 3: return "Apr";
+    break;
+
+    case 4: return "May";
+    break;
+
+    case 5: return "Jun";
+    break;
+
+    case 6: return "Jul";
+    break;
+
+    case 7: return "Aug";
+    break;
+
+    case 8: return "Sep";
+    break;
+
+    case 9: return "Oct";
+    break;
+
+    case 10: return "Nov";
+    break;
+
+    case 11: return "Dec";
+    break;
+
+    default: return "N/A";
+    break;
+
+  }
+}
---
+++ material/js/my-app.js
@@ -0,0 +1,124 @@
+// Initialize your app
+var myApp = new Framework7({
+    panelLeftBreakpoint: 1024
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add views
+var leftView = myApp.addView('.view-left', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+//this works
+$$(document).on('page:init', function (e) {
+
+  var page = e.detail.page;
+
+  if(page.name === 'about') {
+    drawChart();
+  }
+
+  if(page.name === 'services') {
+  }
+
+});
+
+function getMainView() {
+  return mainView;
+}
+
+function drawChart() {
+
+  //remove canvas before drawing the chart
+  $('#myChart').remove();
+
+  //append new canvas to parent
+  $('#contentBlock').append('<canvas id="myChart" width="400" height="385"></canvas>');
+
+  var chart = document.getElementById("myChart");
+  var ctx = document.getElementById("myChart").getContext('2d');
+
+  //Global chart settings
+  Chart.defaults.global.defaultFontSize = 16;
+  Chart.defaults.global.animation.duration = '1000';
+  Chart.defaults.global.animation.easing = 'easeOutSine';
+
+  //chart.height = 700; //works
+  chart.height = $(window).height() - $('#navbar').height() - 50;
+
+  //create chart with parameters
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: getLabels(),
+          datasets: [{
+              label: 'Weight',
+              data: getData(),
+              backgroundColor: [
+                  'rgba(156, 39, 176, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(156,39,176,1)'
+              ],
+              borderWidth: 3,
+              pointRadius: 0,
+              pointHitRadius: 20,
+              cubicInterpolationMode: 'default'
+          }]
+      },
+      options: {
+          maintainAspectRatio: false, //this will make the graph use assigned size
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:false
+                  }
+              }]
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            enabled: true,
+            backgroundColor: 'rgba(255,255,255,1)',
+            titleFontColor: '#000',
+            titleFontSize: 16,
+            bodyFontColor: '#000',
+            bodyFontSize: 16,
+            displayColors: false,
+            borderColor: '#000',
+            borderWidth: 1
+          },
+          annotation: {
+            annotations: [{
+              //drawTime: "afterDatasetsDraw",
+              id: "hline",
+              type: "line",
+              mode: "horizontal",
+              scaleID: "y-axis-0",
+              value: goalWeight,
+              borderColor: "black",
+              borderWidth: 1,
+              borderDash: [2, 2],
+            label: {
+              backgroundColor: "white",
+              content: "Goal Weight",
+              enabled: true,
+              fontColor: "black"
+              }
+
+          }]
+        }
+      }
+  });
+
+  myApp.hidePreloader();
+
+}
