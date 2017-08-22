953a7821a68e3937a431c666a82d6384408a658e
Denis Mylnikov <denismylnikov@MacBook-Pro-Denis.local>
2017-08-22 15:11:00 +0200

    Added checks for all inputs, added loading spinner while calculating chart

--- js/logic.js
+++ js/logic.js
@@ -14,42 +14,23 @@ var data = []; //this needs to be cleared before filling again
 
 //clicking the button calls this
 function calculate() {
+
+  checkPassed = true; //otherwise value will be permanently stuck in false if one of the checks failed
+
   showStartingData();
   readInputValues();
-  //checkValues();
+  checkValues();
+  checkIntake();
   //calculateWeightLoss();
 
   //this doesn't work for some reason
   if(checkPassed) {
+    myApp.showPreloader();
     calculateWeightLoss();
   }
 
   console.log("Labels array: " + labels);
   console.log("Data array: " + data);
-
-  if (typeof Chart != "undefined") {
-    //console.log("chart exists");
-    //ctx.clearRect(0,0,canvas.width, canvas.height);
-  }
-
-  //old chart needs to be destroyed!
-  /*if (document.getElementById("myChart") !== null) {
-    drawChart();
-  }*/
-
-  //check if chart already exists, if not, don't call this
-  if (typeof Chart != "undefined") {
-  }
-
-
-
-  if (typeof Chart == "undefined") {
-    console.log("chart doesn't exist");
-  }
-
-
-
-
 }
 
 //Adds specified number of days to current date
@@ -67,43 +48,105 @@ function showStartingData() {
 }
 
 function readInputValues() {
+
+  //assign all text input fields to variables
   console.log("read input values executed");
   weight = parseFloat(document.getElementById("currentWeightInput").value);
   age = parseFloat(document.getElementById("ageInput").value);
   height = parseFloat(document.getElementById("heightInput").value);
   calorieIntake = parseFloat(document.getElementById("calorieIntakeInput").value);
   goalWeight = parseFloat(document.getElementById("goalWeightInput").value);
+  console.log("Weight: " + weight);
 
+  //get selected gender
   var genderSelector = document.getElementById("genderSelect");
   console.log("Selected gender: " + genderSelector.options[genderSelector.selectedIndex].value);
 
-  //this works
   if (genderSelector.options[genderSelector.selectedIndex].value == "Female") {
     female = true;
   } else {
     female = false;
   }
 
+  //get activity level
   activityLevel = parseInt(getActivityLevel());
   console.log("New activity level is: " + activityLevel);
 
   //alert("Weight: " + weight + " Age: " + age + " Height: " + height + " Calorie intake: " + calorieIntake + " Goal weight: " + goalWeight);
 }
 
+//checks input and throws errors in case some values are inadequate
 function checkValues() {
-  if (weight <= 0) {
-    myApp.alert("Weight cannot be negative or zero.", "Oops!");
+  if (weight < 40 || weight > 600) {
+    myApp.alert("Weight must be between 40 and 600.", "Oops!");
     checkPassed = false;
   }
 
-  if (weight >= 1000) {
-    myApp.alert("You are not THAT big, are you?", "Oops!");
-  }
-
-  if (weight == null || weight == "") {
+  if (weight == null || weight == "" || isNaN(weight)) {
     myApp.alert("Weight input cannot be empty!", "Oops!");
+    checkPassed = false;
   }
 
+  if (age == null || age == "" || isNaN(age)) {
+    myApp.alert("Age input cannot be empty!", "Oops!");
+    checkPassed = false;
+  }
+
+  if (age < 10 || age > 110) {
+    myApp.alert("Age must be between 10 and 110.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (height == null || height == "" || isNaN(height)) {
+    myApp.alert("Height cannot be empty.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (height < 100 || height > 250) {
+    myApp.alert("Height must be between 100 and 250.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (goalWeight > weight) {
+    myApp.alert("Goal weight must be lower than current weight.", "Oops!");
+    checkPassed = false;
+  }
+
+
+
+  //there needs to be a functionality that calculates goal weight based on BMI if field is left empty
+  if (goalWeight == null || goalWeight == "" || isNaN(goalWeight)) {
+    myApp.alert("Goal weight cannot be empty.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (goalWeight < 30 || goalWeight > 500) {
+    myApp.alert("Goal weight must be between 30 and 500.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (calorieIntake < 1000) {
+    myApp.alert("Calorie intake of less than 1000 is very unhealthy, please don't do it.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (calorieIntake > 15000) {
+    myApp.alert("Calorie intake can't be higher than 15000.", "Oops!");
+    checkPassed = false;
+  }
+
+  if (calorieIntake == null || calorieIntake == "" || isNaN(calorieIntake)) {
+    myApp.alert("Calorie intake can't be empty!", "Oops!");
+    checkPassed = false;
+  }
+
+}
+
+function checkIntake() {
+  if (calculateTDEE() < calorieIntake) {
+    myApp.alert("You calorie intake is too high to lose weight. Reduce intake or increase excersise.", "Oops!");
+    checkPassed = false;
+  }
 }
 
 function getActivityLevel() {
@@ -214,7 +257,9 @@ while(parseFloat(weight) >= parseFloat(goalWeight-3)) {
   if (calculateTDEE() <= calorieIntake) {
 				console.log("Your TDEE is now less than calorie intake, this is the lowest weight you can achieve.");
 				console.log("Weight: " +  weight);
+        myApp.alert("You can't reach your goal weight with given calorie intake and level of activity. The lowest weight you can achieve is: " + weight.toFixed(1), "Oops!");
         weight = 0; //this seems to stop the while loop
+
 				//break; //this needs to be here!!
 			}
 
--- js/my-app.js
+++ js/my-app.js
@@ -133,4 +133,7 @@ function drawChart() {
           }
       }
   });
+
+  myApp.hidePreloader();
+
 }
